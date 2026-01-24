eeea15d7b0d6b5f21d7ad922bd2bbd33bb756239
R0p3sh <rup3shrj@gmail.com>
2026-01-24 22:45:52 +0545

    todo-app

---
+++ projects/to-do/todo.js
@@ -0,0 +1,17 @@
+let todoList = [];
+displayItems();
+function addTodo() {
+  let inputElement = document.querySelector('#todo-input');
+  let todoItem = inputElement.value;
+  todoList.push(todoItem);
+  inputElement.value = '';
+  displayItems();
+}
+
+function displayItems() {
+  let displayElement = document.querySelector('#todo-items');
+  displayElement.innerText = '';
+  for(let i = 0; i < todoList.length; i++) {
+    displayElement.innerText = displayElement.innerText + todoList[i];
+  }
+}
