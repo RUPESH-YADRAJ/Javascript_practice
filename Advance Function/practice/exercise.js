2af62610d87d1e38de5e98877d7e198d5669db19
R0p3sh <rup3shrj@gmail.com>
2026-01-27 22:51:38 +0545

    exercise

---
+++ Advance Function/practice/exercise.js
@@ -0,0 +1,19 @@
+  //  let buttonElement = document.querySelector('.btn-subscribe');
+  //  buttonElement.addEventListener('click',event => setTimeout(doubleTheSize, 2000))
+
+  //  function doubleTheSize() {
+  //     let buttonElement = document.querySelector('.btn-subscribe');
+  //     buttonElement.classList.add('size-double');
+  //   }
+
+
+    // let multiply = (num1, num2) => num1 * num2;
+    // console.log(multiply(4,5));
+
+    // let printGreeting = () => console.log('Namaste');
+    // let runTwice = inputFunction => {
+    //   inputFunction();
+    //   inputFunction();
+    // };
+
+    // runTwice(printGreeting);
